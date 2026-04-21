1d679ff67b0aa467ae2cdc9aad1ddecbe2a7ab3b
luis echeverria <Luisecheverria@liceovvh.cl>
2026-04-21 08:10:15 -0400

    avance

---
+++ python/fundamentos/fundamentos/02_estructura_control/11_funciones.py
@@ -0,0 +1,19 @@
+def multiplicacion(num1, num2): #definimos la función multiplación con los parámetros num1 y num2
+   resultado = num1 * num2     #instrucciones dentro de la función
+   return resultado            #regresamos valor de resultado
+
+def buenos_dias(nombre):
+   print("Buenos días "+nombre)
+
+   buenos_dias("alegría")
+buenos_dias("al amor")
+buenos_dias("a la vida")
+buenos_dias("señor Sol")    
+
+def buenos_dias(nombre):
+   return "Buenos días "+nombre
+
+#El valor de retorno de la función es "Buenos días Python", por lo que el valor de mi variable frase será ese
+
+frase = buenos_dias("Python")
+print(frase) #Imprime: Buenos días Python
