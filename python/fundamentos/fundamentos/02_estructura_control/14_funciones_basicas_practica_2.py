9c96b21bde297dcd07a86c67ad8ee6299386930d
luis echeverria <Luisecheverria@liceovvh.cl>
2026-04-22 14:43:14 -0400

    avance

--- python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
+++ python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
@@ -1,45 +1,55 @@
+import os 
+def multiplica_por_2():
+    return [i * 2 for i in range(6)]
 
-# Calcula experiencia
+def suma_y_resta(lista):
+    total = sum(lista)
+    diferencia = abs(lista[0] - lista[1])
+    print(total)
+    return diferencia
 
-def multiplica_por_2(num):
-    result = []
-    for i in range(num + 1):
-        result.append(i * 2)
-    return result
-    result1 = 1
-multiplica_por_2(5)
-# Debe retornar: [0, 2, 4, 6, 8, 10]
+def sumatoria_menos_longitud(lista):
+    return sum(lista) - len(lista)
 
+def valores_multiplicados_segundo(lista):
+    print(len(lista))
+    if len(lista) < 2:
+        return []
+    return [x * lista[1] for x in lista]
 
+def valor_multiplicado_longitud(valor, longitud):
+    return [valor * longitud] * longitud
 
-# Analiza publicaciones
-suma_y_resta([120, 115])
-# Imprime: 235 y retorna: 5
-
-
-
-# Puntaje ajustado
-sumatoria_menos_longitud([10, 5, 3, 7])
-# Suma total = 25, longitud = 4, debe retornar: 21
-
-
-
-# Ajusta visualizaciones
-valores_multiplicados_segundo([100, 3, 50, 20])
-# Imprime: 4 y retorna: [300, 9, 150, 60]
+def limpiar_consola():
+   os.system('cls')
+# --- MENÚ (afuera de las funciones) ---
 
 
+continuar = True
+while continuar:
+ opcion = input("Elige una opción (1-5): ")
+ if opcion == "1":
+    limpiar_consola()
+    print(multiplica_por_2())
 
-valores_multiplicados_segundo([100])
-# Imprime: 1 y retorna: []
+ elif opcion == "2":
+    limpiar_consola()
+    print(suma_y_resta([120, 115]))
 
+ elif opcion == "3":
+    limpiar_consola()
+    print(sumatoria_menos_longitud([10, 5, 3, 7]))
 
+ elif opcion == "4":
+    limpiar_consola()
+    print(valores_multiplicados_segundo([100, 3, 50, 20]))
 
-# Genera precio fijo
-valor_multiplicado_longitud(5, 2)
-# Debe retornar: [10, 10]
-
-
-
-valor_multiplicado_longitud(7, 5)
-# Debe retornar: [35, 35, 35, 35, 35]
+ elif opcion == "5":
+    limpiar_consola()
+    print(valor_multiplicado_longitud(5, 2))
+ elif opcion == "0":
+      print("Saliendo...")
+      continuar = False
+else:
+   limpiar_consola()
+   print("Opcion no Valida, intente otra vez")
