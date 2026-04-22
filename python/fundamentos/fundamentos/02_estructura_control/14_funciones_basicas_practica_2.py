0aa7c999f9150f64e6cdb387dc2f371cbb53b658
luis echeverria <Luisecheverria@liceovvh.cl>
2026-04-22 15:15:13 -0400

    dsgfsdgsdg

--- python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
+++ python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
@@ -1,21 +1,38 @@
 import os 
-def multiplica_por_2():
-    return [i * 2 for i in range(6)]
+def multiplica_por_2(num):
+   resultado = []
+   for i in range(num + 1 ):
+      resultado.append(i * 2)
+   return resultado
+def ejercicio1():
+   resultado = multiplica_por_2(5)
+   print(resultado)
+   
 
 def suma_y_resta(lista):
-    total = sum(lista)
-    diferencia = abs(lista[0] - lista[1])
-    print(total)
-    return diferencia
+    suma = lista[0] + lista[1]
+    resta = lista[0] - lista[1]
+    print(f"suma: {suma}")
+    return resta
 
-def sumatoria_menos_longitud(lista):
-    return sum(lista) - len(lista)
+def ejercicio2():
+    resta = suma_y_resta([120, 115])
+    print(f"resta: {resta}")
+
+
+def sumatoria_menos_longitud(sumatoria):
+   total = sum(sumatoria)
+   longitud = len(sumatoria)
+   resultado = total - longitud
+   print(f"Total = {total}, Longitud = {longitud}")
+   return resultado
+def ejercicio3():
+   retornar = sumatoria_menos_longitud([10, 5, 3, 7])
+   print(f"Resultado: {retornar}")
+
 
 def valores_multiplicados_segundo(lista):
-    print(len(lista))
-    if len(lista) < 2:
-        return []
-    return [x * lista[1] for x in lista]
+   
 
 def valor_multiplicado_longitud(valor, longitud):
     return [valor * longitud] * longitud
@@ -53,3 +70,4 @@ while continuar:
 else:
    limpiar_consola()
    print("Opcion no Valida, intente otra vez")
+
