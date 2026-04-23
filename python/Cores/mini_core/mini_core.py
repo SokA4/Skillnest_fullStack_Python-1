276bb4b4ec70d57106d9e3e88fa0f633fb482465
luis echeverria <Luisecheverria@liceovvh.cl>
2026-04-23 10:28:11 -0400

    fsdfsfgsd

---
+++ python/Cores/mini_core/mini_core.py
@@ -0,0 +1,29 @@
+datos = [
+   {"nombre": "Carlos", "puntaje": 80},
+   {"nombre": "MarIa", "puntaje": 95},
+   {"nombre": "Pedro", "puntaje": 70}
+]
+
+# 1. Cambiar el puntaje de Pedro a 75
+print(datos[2]["nombre"])  # Imprime el nombre de pedro
+datos[2]["puntaje"] = 75
+print(datos[2]["puntaje"])  # Imprime el nuevo puntaje de pedro
+
+# 2. Crear función que imprima:
+#    "Carlos obtuvo 80 puntos"
+
+def puntaje_carlos():
+    for dato in datos:
+        print(f"{dato['nombre']} obtuvo {dato['puntaje']} puntos")
+        break 
+puntaje_carlos()
+
+# 3. Crear función que reciba "nombre" o "puntaje" e imprima solo esos valores
+def imprimir_valores(datos):
+    for dato in datos:
+        print(f"{dato['nombre']} {dato['puntaje']} ")
+imprimir_valores()
+
+
+
+
--- python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
+++ python/fundamentos/fundamentos/02_estructura_control/14_funciones_basicas_practica_2.py
@@ -32,42 +32,76 @@ def ejercicio3():
 
 
 def valores_multiplicados_segundo(lista):
+   if len(lista) < 2:
+      print(len(lista))
+      return []
+   else :
+      segEle = lista[1]
+      nueva_lista = []
+      for num in lista:
+         nueva_lista.append(num * segEle)
+      longitud = len(nueva_lista)
+      print(longitud)
+      return nueva_lista
    
+def ejercicio4():
+   result1 = valores_multiplicados_segundo([100, 3, 50, 20])
+   print(result1)
+   print()
+   result2 = valores_multiplicados_segundo([100])
+   print(result2)
 
-def valor_multiplicado_longitud(valor, longitud):
-    return [valor * longitud] * longitud
+
+
+def valor_multiplicado_longitud(a, b):
+    multiList = []
+    for i in range(0, b):
+       multiList.append(a * b)
+       return multiList
+    
+def ejercicio5():
+   resultado = valor_multiplicado_longitud(5, 2)
+   print(f"Resultado 1: {resultado}")
+   resultado2 = valor_multiplicado_longitud(7, 5)
+   print(f"Resultado 2: {resultado2}")
+
+
 
 def limpiar_consola():
    os.system('cls')
 # --- MENÚ (afuera de las funciones) ---
 
-
+# Menú de navegación para ejercicios
 continuar = True
 while continuar:
- opcion = input("Elige una opción (1-5): ")
- if opcion == "1":
-    limpiar_consola()
-    print(multiplica_por_2())
-
- elif opcion == "2":
-    limpiar_consola()
-    print(suma_y_resta([120, 115]))
-
- elif opcion == "3":
-    limpiar_consola()
-    print(sumatoria_menos_longitud([10, 5, 3, 7]))
-
- elif opcion == "4":
-    limpiar_consola()
-    print(valores_multiplicados_segundo([100, 3, 50, 20]))
-
- elif opcion == "5":
-    limpiar_consola()
-    print(valor_multiplicado_longitud(5, 2))
- elif opcion == "0":
-      print("Saliendo...")
-      continuar = False
-else:
-   limpiar_consola()
-   print("Opcion no Valida, intente otra vez")
+    print("\n--- Ejercicios Python ---")
+    print("--- Ejercicio 1: ---")
+    print("--- Ejercicio 2: ---")
+    print("--- Ejercicio 3: ---")
+    print("--- Ejercicio 4: ---")
+    print("--- Ejercicio 5: ---")
+    opcion = input("\nElige una opción (1-5) (0 para salir) = ")
+    if opcion =="1":
+        limpiar_consola()
+        print("\nEjecutando ejercicio 1: ")
+        ejercicio1()
+    elif opcion =="2":
+        limpiar_consola()
+        print("\nEjecutando ejercicio 2: ")
+        ejercicio2()
+    elif opcion =="3":
+        limpiar_consola()
+        print("\nEjecutando ejercicio 3: ")
+        ejercicio3()
+    elif opcion =="4":
+        limpiar_consola()
+        print("\nEjecutando ejercicio 4: ")
+        ejercicio4()
+    elif opcion =="5":
+        limpiar_consola()
+        print("\nEjecutando ejercicio 5: ")
+        ejercicio5()
+    else:
+        limpiar_consola()
+        print("Opción no válida, intenta otra vez")
 
