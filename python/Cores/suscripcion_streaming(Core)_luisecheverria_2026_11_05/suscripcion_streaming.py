80d7991eae5fde3305309068032f0f968a661d1d
luis echeverria <Luisecheverria@liceovvh.cl>
2026-05-11 12:47:34 -0400

    ghfhgfg

--- python/Cores/suscripcion_streaming(Core)_luisecheverria_2026_11_05/suscripcion_streaming.py
+++ python/Cores/suscripcion_streaming(Core)_luisecheverria_2026_11_05/suscripcion_streaming.py
@@ -20,7 +20,8 @@ class SuscripcionStreaming:
     def __init__(self, usuario, tipo_suscripcion="Gratis"):
         self.usuario = usuario
         self.tipo_suscripcion = tipo_suscripcion
-        self.saldo_pendiente = 0
+        self.costo_mensual = self.costos_suscripcion(tipo_suscripcion)
+        self.saldo_pendiente = self.costo_mensual
 
     def realizar_pago(self, monto):
         """Reduce el saldo pendiente según el monto pagado."""
@@ -47,5 +48,32 @@ class SuscripcionStreaming:
        
 
     def mostrar_info_suscripcion(self):
-       """Muestra la información de la suscripción del usuario."""
-       pass
+        """Muestra la información de la suscripción del usuario."""
+        return (
+            f"Usuario: {self.usuario}, "
+            f"Tipo de Suscripción: {self.tipo_suscripcion}, "
+            f"Costo Mensual: {self.costo_mensual}, "
+            f"Saldo Pendiente: {self.saldo_pendiente}")
+        
+
+Dany = SuscripcionStreaming("Dany", "Estándar")
+Luis = SuscripcionStreaming("Luis", "Premium")
+Constanza = SuscripcionStreaming("Constanza", "Gratis")
+
+"""Crea 3 usuarios con diferentes tipos de suscripción.
+Haz que el primer usuario intente ver contenido exclusivo, mejore su suscripción y pague su saldo.
+Haz que el segundo usuario vea contenido exclusivo, cambie su suscripción a Premium y pague dos veces.
+Haz que el tercer usuario intente pagar una cantidad menor a su saldo pendiente y vea contenido exclusivo."""
+
+print(Dany.ver_contenido_exclusivo())
+Dany.cambiar_suscripcion("Premium")
+print(Dany.mostrar_info_suscripcion())
+Dany.realizar_pago(10.99)
+
+print(Luis.ver_contenido_exclusivo())
+Luis.cambiar_suscripcion("Premium")
+print(Luis.mostrar_info_suscripcion())
+Luis.realizar_pago(10.99)
+
+print(Constanza.mostrar_info_suscripcion())
+Constanza.realizar_pago(0.99)
